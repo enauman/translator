41a24b04405d9dbd6e35690c72cde505313552f5
Erik Nauman <enauman@hewittschool.org>
2023-09-19 20:15:09 -0400

    add comments

--- voice_service.py
+++ voice_service.py
@@ -1,14 +1,15 @@
 import os
 import speech_recognition as sr
 import threading
-from language_selector import Language_Selector
-ls = Language_Selector()
+import time
 r = sr.Recognizer()
 r.pause_threshold = 0.8
 r.energy_threshold = 4000
 r.dynamic_energy_threshold = True
-#r.operation_timeout = 2.0
+#r.operation_timeout = 1
 mic = sr.Microphone(device_index=0)
+path = '/home/translator/app/'
+
 class Voice_Service:
 	def __init__(self,running):
 		self.index = 0
@@ -24,19 +25,22 @@ class Voice_Service:
 	def listen(self,language):
 		self.listening = True
 		try:
+			#listen until pause
 			with mic as source:
 				r.adjust_for_ambient_noise(source, duration = 0.5)
-				self.audio = r.listen(source, timeout = 1.0)
+				self.audio = r.listen(source, timeout = 0.5)
+			#start transcription and translation as thread, go back to listen again
 			translate = threading.Thread(target=self.transcribe, args=(1,language), daemon=True)
 			translate.start()
-			# self.transcribe(language)
 		except sr.WaitTimeoutError as e:
 			print(e)
 		self.listening = False
 
 	def transcribe(self,name,language):
+		global path
 		text = "..."
 		try:
+			#recognize_google_cloud supposed to be for production but not as good?
 #			text = r.recognize_google_cloud(self.audio, credentials_json="/home/translator/app/application_default_credentials.json")
 			text = r.recognize_google(self.audio)
 			if text == "shut down":
@@ -45,10 +49,11 @@ class Voice_Service:
 			text = "could not recognize"
 		finally:
 			print(text)
+		#if speech recognized add to transcription list and full transcription file 
 		if text != "could not recognize":
 			self.transcription.append(text)
 			try:
-				with open('translated.txt', 'a') as af:
+				with open(path + 'translated.txt', 'a') as af:
 					af.writelines(self.transcription[self.index])
 					af.write("\n")
 				self.line_to_buffer(language)
@@ -56,36 +61,43 @@ class Voice_Service:
 				print(e)
 
 	def line_to_buffer(self,language):
+		global path
+		# index keeps track of which transcription line to send to buffer 
+		# index increments after translation is done
 		self.buffer = self.transcription[self.index]
 		try:
-			with open('buffer.txt', 'w') as wf:
+			with open(path + 'buffer.txt', 'w') as wf:
 				wf.write(self.buffer)
 		except IOError as e:
 			print(e)
 		self.translate_to_output(language)
 
 	def translate_to_output(self,language):
+		global path
 		lines = []
+		#system call to translate-shell; input is buffer.txt, output to op.txt
 		if language == "es":
-			os.system("trans :es file://buffer.txt -o op.txt")
+			os.system("trans :es file:///home/translator/app/buffer.txt -o /home/translator/app/op.txt")
 		elif language == "ru":
-			os.system("trans :ru file://buffer.txt -o op.txt")
+			os.system("trans :ru file:///home/translator/app/buffer.txt -o /home/translator/app/op.txt")
 		elif language == "uz":
-			os.system("trans :uz file://buffer.txt -o op.txt")
+			os.system("trans :uz file:///home/translator/app/buffer.txt -o /home/translator/app/op.txt")
 		elif language == "bn":
-			os.system("trans :bn file://buffer.txt -o op.txt")
-
+			os.system("trans :bn file:///home/translator/app/buffer.txt -o /home/translator/app/op.txt")
+		#increment index to get next line to translate on next pass
 		self.index += 1
 		try:
-			with open('op.txt', 'r') as rf:
+			#add translation to full transcript
+			with open(path + 'op.txt', 'r') as rf:
 				lines = rf.readlines()
 				print(lines)
-			with open('translated.txt', 'a') as af:
+			with open(path + 'translated.txt', 'a') as af:
 				af.writelines(lines)
 		except IOError as e:
 			print(e)
 		try:
-			with open('print.txt', 'a') as af:
+			# add line to be printed to matrix
+			with open(path + 'print.txt', 'a') as af:
 				af.write(language)
 				af.write('\n')
 				af.writelines(lines)
