51b5f4218fef11a2b996e6bcaa291b6a34ca3c1a
Erik Nauman <enauman@hewittschool.org>
2023-09-19 20:23:49 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from rgb_matrix import RGB_MATRIX
-matrix = RGB_MATRIX()
-
-if __name__ == "__main__":
-	while True:
-		if not matrix.get_scrolling_state():
-			matrix.scroll_text_test(1,"ru","a b c d e")
